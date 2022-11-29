7ccf9416c37574d6d40b062b7ddbf34ae0bbb703
TheRealSpaghetti <gudetamagudetamagudetama@gmail.com>
2022-11-28 22:42:20 -0500

    Initial Commit

---
+++ iOS_Fitness_AppUITests/iOS_Fitness_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_Fitness_AppUITestsLaunchTests.swift
+//  iOS_Fitness_AppUITests
+//
+//  Created by Leon Lee on 2022-11-28.
+//
+
+import XCTest
+
+final class iOS_Fitness_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
